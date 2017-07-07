ee4c91a219fd692fcca7452991e279cae1d371f7
Sai sri vatsava Guntupalli <saisrivatsava.g@outlook.com>
2017-07-07 10:02:21 +0530

    added some programs in regex

---
+++ src/main/scala/com/saisrivatsava/regex/examples/groupingExample.scala
@@ -0,0 +1,5 @@
+package com.saisrivatsava.regex.examples
+
+object groupingExample {
+  
+}
---
+++ src/main/scala/com/saisrivatsava/regex/examples/verifyingEmailAddresses.scala
@@ -0,0 +1,19 @@
+package com.saisrivatsava.regex.examples
+
+import scala.util.matching.Regex
+
+object verifyingEmailAddresses {
+  def main(args: Array[String]): Unit = {
+
+    val pattern = new Regex("[a-zA-Z0-9_]+@[a-zA-Z0-9]+.(com|org|net)")
+    val inputMailsList = List("sai@gmail.com", "bob@outlook.net", "happy.com", "foo@gmail.xyz", "jane_doe@hotmail.com")
+    val acceptedMails = collection.mutable.HashSet("accepted mails")
+    val rejectedMails = collection.mutable.HashSet("rejected mails")
+
+    inputMailsList.foreach { mail => if (pattern.findAllIn(mail).size > 0) acceptedMails += mail else rejectedMails += mail }
+
+    acceptedMails.foreach(println)
+    println()
+    rejectedMails.foreach(println)
+  }
+}
